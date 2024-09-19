5068dee5c3caa909cfe96112bcbc61839d657883
unknown <bishalkandi859494@gmail.com>
2024-09-20 00:35:53 +0530

    seo

--- app/layout.js
+++ app/layout.js
@@ -6,18 +6,18 @@ const inter = Poppins({ subsets: ["latin"], weight:["400", "500","600","700","80
 export const metadata = {
   title: 'Blured UI',
   description: 'Blured UI is a versatile UI component builder inspired by Shad CN, designed for rapid prototyping and seamless integration in Next.js applications.',
-  keywords: 'Blured UI, UI component builder, Next.js, Shad CN, design system, web development, customizable components',
+  keywords: 'Blured UI,blured ui , blureduicomponentbuilder, UI component builder, Next.js, Shad CN, design system, web development, customizable components',
   openGraph: {
     title: 'Blured UI',
     description: 'Create stunning, customizable UI components effortlessly with Blured UI. Perfect for Next.js developers.',
-    images: [], 
+    images: ['https://bisxxal.netlify.app/blogo.png'], 
     url: 'https://blured-ui.vercel.app/',
     site_name: 'Blured UI',
   },
   twitter: {
     title: 'Blured UI',
     description: 'Elevate your web projects with Blured UI, the intuitive UI component builder for Next.js.',
-    images: [], 
+    images: ['https://bisxxal.netlify.app/blogo.png'], 
     card: 'summary_large_image',
     creator: 'bishal',
   }
